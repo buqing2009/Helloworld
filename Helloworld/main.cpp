abb1e34cc1c954d45ae06e6de0ae1e90d9f3756a
步青 <buqing2009@buqingdeMacBook-Pro.local>
2015-02-15 19:44:09 +0800

    Initial Commit

---
+++ Helloworld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Helloworld
+//
+//  Created by 步青 on 15/2/15.
+//  Copyright (c) 2015年 步青. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
